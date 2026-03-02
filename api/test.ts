afbe976f0da7c752e8b13b2b3a2846051cfd1feb
Guica7 <guillerhaya7@gmail.com>
2026-03-02 14:27:41 +0100

    Cleanup: Remove diagnostic test API

--- api/test.ts
+++
@@ -1,7 +0,0 @@
-
-export default async function handler(req: Request) {
-    return new Response(JSON.stringify({ status: 'ok', time: new Date().toISOString() }), {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' }
-    });
-}
